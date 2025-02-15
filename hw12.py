481b56e18ebea3b49eed79fa5a8952f2cea61af2
OrangeTopHat <JekaFunSteve2@gmail.com>
2025-02-15 10:30:40 +0100

    Update hw12.py

--- hw12.py
+++ hw12.py
@@ -2,18 +2,22 @@ import requests
 from bs4 import BeautifulSoup
 from collections import Counter
 
-url = input(print("Введіте посилання:")) # Укажите нужную страницу
-word_to_count = input(print("Введіть слово:"))  # Слово, которое нужно подсчитать
+url = input(print("Введіте посилання:"))
+if url == "":
+    raise TypeError
+else:
+    word_to_count = input(print("Введіть слово:"))
+    if word_to_count == "":
+        raise TypeError
+    else:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
+        text = soup.get_text()
 
-response = requests.get(url)
-soup = BeautifulSoup(response.text, "html.parser")
-text = soup.get_text()
+        words = text.lower().split()
+        word_count = Counter(words)
 
-words = text.lower().split()
-word_count = Counter(words)
-
-print(f"Слово '{word_to_count}' зустрічається {word_count[word_to_count]} раз(а).")
-
+        print(f"Слово '{word_to_count}' зустрічається {word_count[word_to_count]} раз(а).")
 
 
 
